990628a73a930830b98e785e0c23732f1fce5fa1
Diego Dantas Figueiredo <35031027+Feelss@users.noreply.github.com>
2018-07-10 13:17:55 -0300

    Bug fixes & new functions

--- VicePyBot/enquete.py
+++ VicePyBot/enquete.py
@@ -3,10 +3,14 @@ def enq(message):
     type = message.text.split()[1]
     if type == 'create':
         create(message)
+    elif type == 'add_option':
+        entries(message)
     elif type == 'show':
         showEnquete(message)
     elif type == 'vote':
-        #implementar
+        votar(message)
+    elif type == 'end':
+        encerrar(message)
     else:
         bot.send_message(message.chat.id, 'Comando nao reconhecido, insira um comando valido.')
 
@@ -15,23 +19,63 @@ def create(message):
     msg = bot.reply_to(message, 'Tópico da enquete?', reply_markup = markup)
     del enquete[:]
     enquete.append('--- ' + msg.text + ' ---\n')
-    bot.register_next_step_handler(msg, entries)
 
 def entries(message):
+    if len(enquete) == 0:
+        ans = 'Sem enquete criada'
+    else:
+        markup = types.ForceReply()
+        msg = bot.reply_to(message, 'What do you want?', reply_markup = markup)
+        enquete.append(msg.text)
+        ans = 'Adicionado com sucesso!'
+        results.append(0)
+    bot.send_message(message.chat.id, ans)
+
+def votar(message):
     markup = types.ForceReply()
-    while True:
-        msg = bot.reply_to(message, 'Adicionar opçao na enquete? (Digite -1 caso nao queira)', reply_markup = markup)
-        if msg == -1:
-            bot.send_message(message.chat.id, 'Enquete criada com sucesso!')
-            break
+    msg = bot.reply_to(message, 'Vote limpo', reply_markup=markup)
+    try:
+        idx = int(msg.text)
+        if idx >= len(enquete):
+            results[idx] += 1
+            ans = 'Voto na opção ' + str(idx) + ' adicionado com sucesso'
         else:
-            enquete.append(msg.text)
+            ans = 'Opção invalida'
+
+    except ValueError:
+        ans = 'Opção invalida'
+    bot.send_message(message.chat.id, ans)
+
+def encerrar(message):
+    if len(enquete) == 0:
+        ans = 'Sem enquete ativa.'
+    else:
+        ans = '-- Competidores! -- \n'
+        ans += opcoes()
+        ans = '-- RESULTADO FINAL -- \n'
+        ans += resultado()
+    bot.send_message(message.chat.id, ans)
+
+
+
+def resultado():
+    ans = ''
+    for i in range(len(results)):
+        ans += str(i) + '.' + str(results) + ' votos\n'
+    return ans
+
+def opcoes():
+    ans = ''
+    for i in range(len(enquete)):
+        ans += str(i) + '.' + enquete[i] + '\n'
+    return ans
+    
 
 def showEnquete(message):
     if len(enquete) == 0:
-        bot.send_message(message.chat.id, 'Sem enquetes no momento...')
+        ans = 'Sem enquetes no momento...'
     else:
-        ans = ''
-        for i in range(len(enquete)):
-            ans += str(i) + '.' + enquete[i] + '\n'
-        bot.send_message(message.chat.id, ans)
+        ans = opcoes()
+        ans += '--Resultado parcial--\n'
+        ans += resultado()
+    bot.send_message(message.chat.id, ans)
