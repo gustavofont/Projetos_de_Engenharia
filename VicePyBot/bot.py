e8f28faa16d926f635be7b7d06802e081f04dddd
Gustavo Machado <36394921+gustavofont@users.noreply.github.com>
2018-07-11 21:18:34 -0300

    Update bot.py

--- VicePyBot/bot.py
+++ VicePyBot/bot.py
@@ -2,7 +2,7 @@ import telebot
 from telebot import types
 import datetime
 
-#bot = telebot.Telebot('TOKEN')
+bot = telebot.TeleBot('Token')
 avisos = []
 enquete = []
 results = []
@@ -19,6 +19,9 @@ def send_welcome(message):
     if text != '':
         avisos.append('[' + d + ']: ' + text)
 
+@bot.message_handler(commands=['list'])
+def funções(message):
+    bot.send_message(message.chat.id, ' ---FUNÇÕES--- \n /add. : adicionar um elemento ao quadro de avisos \n /limpar. : apaga todo o conteúdo do quadro de avisos \n /remover. : remove uma notícia pelo índice no quadro de avisos \n /board. : mostra o quadro de avisos \n /enquete create. : cria uma enquete \n /enquete add_option. : adiciona uma opção(cadidado) a enquete \n /enquete show. : mostra o andamento da enquete \n /enquete vote. : adiciona um voto ao candidato escolhido \n /enquete end. : finaliza a enquete')
 
 @bot.message_handler(commands=['limpar'])
 def limpar(message):
@@ -54,48 +57,55 @@ def news_board(message):
 
 @bot.message_handler(commands=['enquete'])
 def enq(message):
-    type = message.text.split()[1]
-    if type == 'create':
-        create(message)
-    elif type == 'add_option':
-        entries(message)
-    elif type == 'show':
+    select = message.text.split()
+    try:
+        tipo = select[1]
+    except Exception as e:
+        bot.send_message(message.chat.id, 'ERROOOU')
+        return
+    if tipo == 'create':
+        try:
+            zerotwo = select[2]
+            enquete.append(zerotwo)
+        except Exception as e:
+            bot.send_message(message.chat.id, 'ERROOOU')
+    elif tipo == 'add_option':
+        try:
+            zerotwo = select[2]
+            entries(message, zerotwo)
+        except Exception as e:
+            bot.send_message(message.chat.id, 'ERROOOU')
+    elif tipo == 'show':
         showEnquete(message)
-    elif type == 'vote':
-        votar(message)
-    elif type == 'end':
+    elif tipo == 'vote':
+        try:
+            zerotwo = select[2]
+            votar(message, zerotwo)
+        except Exception as e:
+            bot.send_message(message.chat.id, 'ERROOOU')
+    elif select[1] == 'end':
         encerrar(message)
     else:
         bot.send_message(message.chat.id, 'Comando nao reconhecido, insira um comando valido.')
 
-def create(message):
-    markup = types.ForceReply()
-    msg = bot.reply_to(message, 'Tópico da enquete?', reply_markup = markup)
-    del enquete[:]
-    enquete.append('--- ' + msg.text + ' ---\n')
-
-def entries(message):
+def entries(message, s):
     if len(enquete) == 0:
         ans = 'Sem enquete criada'
     else:
-        markup = types.ForceReply()
-        msg = bot.reply_to(message, 'What do you want?', reply_markup = markup)
-        enquete.append(msg.text)
+        enquete.append(s)
         ans = 'Adicionado com sucesso!'
         results.append(0)
     bot.send_message(message.chat.id, ans)
 
-def votar(message):
-    markup = types.ForceReply()
-    msg = bot.reply_to(message, 'Vote limpo', reply_markup=markup)
+def votar(message, idx):
+    ans = ''
     try:
-        idx = int(msg.text)
-        if idx >= len(enquete):
+        idx = int(idx)
+        if idx < len(results):
             results[idx] += 1
             ans = 'Voto na opção ' + str(idx) + ' adicionado com sucesso'
         else:
             ans = 'Opção invalida'
-
     except ValueError:
         ans = 'Opção invalida'
     bot.send_message(message.chat.id, ans)
@@ -104,10 +114,12 @@ def encerrar(message):
     if len(enquete) == 0:
         ans = 'Sem enquete ativa.'
     else:
-        ans = '-- Competidores! -- \n'
+        ans = '--- Opções --- \n'
         ans += opcoes()
-        ans = '-- RESULTADO FINAL -- \n'
+        ans += '-- RESULTADO FINAL -- \n'
         ans += resultado()
+        del results[:]
+        del enquete[:]
     bot.send_message(message.chat.id, ans)
 
 
@@ -115,23 +127,23 @@ def encerrar(message):
 def resultado():
     ans = ''
     for i in range(len(results)):
-        ans += str(i) + '.' + str(results) + ' votos\n'
+        ans += enquete[i + 1] + '. ' + str(results[i]) + ' votos\n'
     return ans
 
 def opcoes():
     ans = ''
-    for i in range(len(enquete)):
+    for i in range(1, len(enquete)):
         ans += str(i) + '.' + enquete[i] + '\n'
     return ans
-    
+
 
 def showEnquete(message):
     if len(enquete) == 0:
         ans = 'Sem enquetes no momento...'
     else:
+        ans = '--- ' + enquete[0] + ' ---\n'
         ans = opcoes()
-        ans += '--Resultado parcial--\n'
+        ans += '\n---Resultado parcial---\n'
         ans += resultado()
     bot.send_message(message.chat.id, ans)
-
 bot.polling()
